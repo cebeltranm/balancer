c4d81fd0805e29f297b02823d850387d4e23c667
Carlos Beltran <cebeltranm@gmail.com>
2025-06-12 20:50:17 -0500

    Updates

--- sw.js
+++ sw.js
@@ -81,17 +81,17 @@ define(['./workbox-e28b2a22'], (function (workbox) { 'use strict';
     "url": "assets/auto-C3mEb9gQ.js",
     "revision": null
   }, {
-    "url": "assets/index-gN6KZqF_.js",
+    "url": "assets/index-muh252Di.js",
     "revision": null
   }, {
     "url": "registerSW.js",
     "revision": "2a272aeee910100ba224f9b19acf5d58"
   }, {
-    "url": "assets/index-CqQIUFAk.css",
+    "url": "assets/index-HPLB9xa_.css",
     "revision": null
   }, {
     "url": "index.html",
-    "revision": "38e058c9bef70684bd0399473f919cbd"
+    "revision": "f29bd8336ea2f238ee78e0e933db2345"
   }, {
     "url": "test.html",
     "revision": "b1b8ccfc7f689f3a1650d83865ff523e"
